eea64f6a591918722c1e05ec048c69bb88ffba82
MuzaffarO <muzaffarobidjonov0304@gmail.com>
2023-05-09 14:15:39 +0500

    Changed spring version to 3.0.6

--- src/main/java/uz/nt/articlepublishingservice/dto/UsersDto.java
+++ src/main/java/uz/nt/articlepublishingservice/dto/UsersDto.java
@@ -1,13 +1,22 @@
 package uz.nt.articlepublishingservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsersDto {
     private Integer id;
     private String username;
     private String password;
     private String bio;
     private LocalDateTime createdAt;
-
+    private LocalDateTime updatedAt;
 }
--- src/main/java/uz/nt/articlepublishingservice/model/Users.java
+++ src/main/java/uz/nt/articlepublishingservice/model/Users.java
@@ -1,4 +1,21 @@
 package uz.nt.articlepublishingservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
 public class Users {
+    private Integer id;
+    private String username;
+    private String password;
+    private String bio;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
