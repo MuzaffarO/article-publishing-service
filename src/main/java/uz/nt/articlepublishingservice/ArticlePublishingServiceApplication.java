d4c3693d281df385d4c0fb27595c7fa1733b4c17
MuzaffarO <muzaffarobidjonov0304@gmail.com>
2023-05-09 12:16:33 +0500

    Initial stage

---
+++ src/main/java/uz/nt/articlepublishingservice/ArticlePublishingServiceApplication.java
@@ -0,0 +1,13 @@
+package uz.nt.articlepublishingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlePublishingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticlePublishingServiceApplication.class, args);
+    }
+
+}
