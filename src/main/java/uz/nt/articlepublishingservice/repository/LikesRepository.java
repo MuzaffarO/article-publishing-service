21e76cc9446361afe3124eaface857cf0730346f
Bek <120727408+khudoyorov@users.noreply.github.com>
2023-05-09 15:44:13 +0500

    Follows interface

--- src/main/java/uz/nt/articlepublishingservice/repository/LikesRepository.java
+++ src/main/java/uz/nt/articlepublishingservice/repository/LikesRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import uz.nt.articlepublishingservice.model.Likes;
 
 public interface LikesRepository extends JpaRepository<Likes,Integer> {
+
 }
