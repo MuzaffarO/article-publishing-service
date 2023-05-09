830131229bedd3ab9d897bf1a7bd7a8ac618c345
MuzaffarO <muzaffarobidjonov0304@gmail.com>
2023-05-09 14:33:05 +0500

    AppStatus update

--- src/main/java/uz/nt/articlepublishingservice/model/Users.java
+++ src/main/java/uz/nt/articlepublishingservice/model/Users.java
@@ -17,6 +17,7 @@ public class Users {
     private Integer id;
     private String username;
     private String password;
+    private String email;
     private String bio;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
