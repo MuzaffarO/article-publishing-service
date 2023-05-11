b75900ce5e74eccfffbcbca2ddbfd717d8cf02c5
khudoyorov <boburbekkhudoyorov@gmail.com>
2023-05-11 07:38:17 -0700

    update

--- src/main/java/uz/nt/articlepublishingservice/service/FollowsService.java
+++
@@ -1,9 +0,0 @@
-package uz.nt.articlepublishingservice.service;
-
-import uz.nt.articlepublishingservice.dto.FollowsDto;
-import uz.nt.articlepublishingservice.dto.ResponseDto;
-
-public interface FollowsService {
-    ResponseDto<FollowsDto> follow(FollowsDto followsDto);
-
-}
