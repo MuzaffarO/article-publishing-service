4060f882350a51de3c7aecd2a55dd338fe96e396
MahmudSaidxonov <saidxonovmaxmudjon@gmail.com>
2023-05-15 15:33:21 +0500

    Swagger

--- src/main/java/uz/nt/articlepublishingservice/rest/ArticleResources.java
+++ src/main/java/uz/nt/articlepublishingservice/rest/ArticleResources.java
@@ -1,5 +1,8 @@
 package uz.nt.articlepublishingservice.rest;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,31 +19,118 @@ import uz.nt.articlepublishingservice.service.impl.TagServiceImpl;
 public class ArticleResources {
     private final ArticlesServiceImpl articlesService;
     private final TagServiceImpl tagService;
+
+    @Operation(
+            summary = "Adds a new article",
+            method = "POST",
+            description = "Need to send ArticlesDto to this endpoint to create new article",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.
+                    RequestBody(description = "Article info",
+                    content = @Content(mediaType = "application/json")),
+            responses = {@ApiResponse(responseCode = "200", description = "OK")}
+    )
     @PreAuthorize("hasAnyAuthority('ROLE_USER')")
     @PostMapping
     public ResponseEntity<?> add(@RequestBody ArticlesDto articlesDto){
         return articlesService.add(articlesDto);
     }
+
+    @Operation(
+            summary = "Gets all articles",
+            method = "GET",
+            description = "Get all articles",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.
+                    RequestBody(description = "Article info",
+                    content = @Content(mediaType = "application/json")),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "404", description = "Articles not found")
+            }
+    )
     @GetMapping
     public ResponseEntity<?> getAll(){
         return articlesService.getAll();
     }
+
+    @Operation(
+            summary = "Update article",
+            method = "PATCH",
+            description = "Need to send ArticleDto to this endpoint to article",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.
+                    RequestBody(description = "Article info",
+                    content = @Content(mediaType = "application/json")),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "404", description = "Article not found")
+            }
+    )
     @PatchMapping
     public ResponseEntity<?> update(@RequestBody ArticlesDto articlesDto){
         return articlesService.update(articlesDto);
     }
+
+    @Operation(
+            summary = "Delete a article by article ID",
+            method = "DELETE",
+            description = "Need to send the article ID to delete the article",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.
+                    RequestBody(description = "Article info",
+                    content = @Content(mediaType = "application/json")),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "400", description = "Invalid id supplied"),
+                    @ApiResponse(responseCode = "404", description = "Article not found")
+            }
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Integer id){
         return articlesService.delete(id);
     }
+
+    @Operation(
+            summary = "Gets a article by article ID",
+            method = "GET",
+            description = "Need to send the article ID to get this article",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.
+                    RequestBody(description = "Article info",
+                    content = @Content(mediaType = "application/json")),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "400", description = "Invalid id supplied"),
+                    @ApiResponse(responseCode = "404", description = "Article not found")
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<?> get(@PathVariable Integer id){
         return articlesService.get(id);
     }
+
+    @Operation(
+            summary = "Gets popular tags",
+            method = "GET",
+            description = "Get all popular tags",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.
+                    RequestBody(description = "Tags info",
+                    content = @Content(mediaType = "application/json")),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "404", description = "Tags not found")
+            }
+    )
     @GetMapping("/popular-tags")
     public ResponseEntity<?> popularTags(){
         return tagService.popularArticles();
     }
+
+    @Operation(
+            summary = "Like",
+            method = "POST",
+            description = "Like the article",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.
+                    RequestBody(description = "Like info",
+                    content = @Content(mediaType = "application/json")),
+            responses = {@ApiResponse(responseCode = "200", description = "OK")}
+    )
     @PostMapping("/like/{articleId}")
     public ResponseEntity<?> like(@PathVariable Integer articleId){
         return articlesService.like(articleId);
--- src/main/java/uz/nt/articlepublishingservice/rest/ImageResources.java
+++ src/main/java/uz/nt/articlepublishingservice/rest/ImageResources.java
@@ -1,5 +1,8 @@
 package uz.nt.articlepublishingservice.rest;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,11 +18,33 @@ public class ImageResources {
 
     private final ImageService imageService;
 
+    @Operation(
+            summary = "Save user image in three formats",
+            method = "POST",
+            description = "Need to send image",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.
+                    RequestBody(description = "File info",
+                    content = @Content(mediaType = "multipart/form-data")),
+            responses = {@ApiResponse(responseCode = "200", description = "OK")}
+    )
     @PostMapping("upload")
     public ResponseDto<Integer> uploadFile(@RequestParam Integer userId, @RequestPart("file") MultipartFile file){
         return imageService.fileUpload(userId, file);
     }
 
+    @Operation(
+            summary = "Gets a image by image ID",
+            method = "GET",
+            description = "Need to send the image ID and size (LARGE, MEDIUM, SMALL) to receive this image",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.
+                    RequestBody(description = "File info",
+                    content = @Content(mediaType = "application/json")),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "OK"),
+                    @ApiResponse(responseCode = "400", description = "Invalid id or size supplied"),
+                    @ApiResponse(responseCode = "404", description = "File not found")
+            }
+    )
     @GetMapping("get-user-image")
     public ResponseDto<byte[]> getFileById(@RequestParam Integer userId,@RequestParam String size) throws IOException {
         return imageService.getFileById(userId, size);
